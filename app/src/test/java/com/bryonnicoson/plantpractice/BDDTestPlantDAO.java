bafffd280851a6083f2982b477838a6f81933375
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-02-22 11:44:23 -0600

    More Asserts - Given When Then

--- app/src/test/java/com/bryonnicoson/plantpractice/BDDTestPlantDAO.java
+++ app/src/test/java/com/bryonnicoson/plantpractice/BDDTestPlantDAO.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.List;
 
 import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Created by bryon on 2/22/18.
@@ -48,4 +49,52 @@ public class BDDTestPlantDAO {
         // did we find a redbud?
         assertTrue(redbudFound);
     }
+
+    @Test
+    public void testPlantDAO_fetchShouldReturnAtLeastTwoOaksForQuercus() throws IOException, JSONException {
+        givenPlantDAOIsInitialized();
+        whenSearchForQuercus();
+        thenVerifyTwoOaks();
+    }
+
+    private void whenSearchForQuercus() throws IOException, JSONException {
+        plants = plantDAO.fetchPlants("Quercus");
+    }
+
+    private void thenVerifyTwoOaks() {
+        boolean quercusRoburFound = false;
+        boolean quercusAlbaFound = false;
+
+        for (PlantDTO plant : plants) {
+            if (plant.getGenus().equals("Quercus") && plant.getSpecies().equals("robur") && plant.getCommon().contains("Oak")) {
+                quercusRoburFound = true;
+            }
+        }
+        assertTrue(quercusRoburFound);
+
+        for (PlantDTO plant : plants) {
+            if (plant.getGenus().equals("Quercus") && plant.getSpecies().equals("alba") && plant.getCommon().contains("Oak")) {
+                quercusAlbaFound = true;
+            }
+        }
+        assertTrue(quercusAlbaFound);
+    }
+
+    @Test
+    public void testPlantDAO_fetchSHouldReturnNothingForJibberish() throws IOException, JSONException {
+        givenPlantDAOIsInitialized();
+        whenSearchForJibberish();
+        thenVerifyNoResults();
+    }
+
+    private void whenSearchForJibberish() throws IOException, JSONException {
+        plants = plantDAO.fetchPlants("sklujap");
+    }
+
+    private void thenVerifyNoResults() {
+        int size = plants.size();
+        assertEquals(0, size);
+    }
+
+
 }
